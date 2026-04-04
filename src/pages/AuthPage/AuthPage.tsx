eaa2dcb03bae65ab9a70bd2c278f07a022f97b27
DmitriyGulevich <kaskodim@gmail.com>
2026-04-04 19:45:13 +0300

    добавляет иконки страницы формы + стилизация контейнера формы

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 import { useLoginByCredentialsMutation } from "@/shared/api/authApi.ts";
+
+import "./AuthPage.styles.css";
 import { Icon } from "@/shared/ui/Icon/Icon.tsx";
 
 type Credentials = {
@@ -43,50 +45,59 @@ export const AuthPage = () => {
   };
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        gap: 3,
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <div>
-        <Icon name={"logo"} width="30" height="30" />
-      </div>
-      <div>Добро пожаловать!</div>
-      <div>Пожалуйста авторизуйтесь</div>
-      {/*==================================================*/}
-      <div>Логин</div>
-      <input
-        value={credentials.login}
-        onChange={(e) =>
-          setCredentials((prev) => ({ ...prev, login: e.target.value }))
-        }
-      />
-      <div>Пароль</div>
-      <input
-        value={credentials.password}
-        type={"password"}
-        onChange={(e) =>
-          setCredentials((prev) => ({ ...prev, password: e.target.value }))
-        }
-      />
+    <div className="container">
+      <div className="form-outer">
+        <div className="form-border">
+          <div className="form">
+            {/*================= Иконка =================================*/}
+            <div>
+              <Icon name={"logo"} size={30} />
+            </div>
+            {/*================== Приветствие ===============================*/}
+            <div>
+              <div>Добро пожаловать!</div>
+              <div>Пожалуйста авторизуйтесь</div>
+            </div>
+            {/*=================== Форма ==================================*/}
+            <div>
+              <div>Логин</div>
+              <input
+                value={credentials.login}
+                onChange={(e) =>
+                  setCredentials((prev) => ({ ...prev, login: e.target.value }))
+                }
+              />
+              <div>Пароль</div>
+              <input
+                value={credentials.password}
+                type={"password"}
+                onChange={(e) =>
+                  setCredentials((prev) => ({
+                    ...prev,
+                    password: e.target.value,
+                  }))
+                }
+              />
 
-      {/*====================================================*/}
-      <div style={{ display: "flex", gap: 3 }}>
-        <div>чекбокс</div>
-        <div>запомнить данные</div>
-      </div>
+              <div style={{ display: "flex", gap: 3 }}>
+                <div>чекбокс</div>
+                <div>запомнить данные</div>
+              </div>
 
-      <button onClick={() => handleClick(credentials)}>Войти</button>
-      <div>или</div>
-      <div style={{ display: "flex", gap: 3 }}>
-        <div>Нет аккаунта?</div>
-        <a href={""}>Создать</a>
+              <button onClick={() => handleClick(credentials)}>Войти</button>
+              <div>или</div>
+            </div>
+
+            {/*==================== Подвал ==============================*/}
+
+            <div style={{ display: "flex", gap: 3 }}>
+              <div>Нет аккаунта?</div>
+              <a href={""}>Создать</a>
+            </div>
+
+            {/*  =============================================================*/}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,20 +1,45 @@
-import type { SVGProps } from "react";
+import type { ComponentType, HTMLAttributes, SVGProps } from "react";
 import Logo from "./assets/logo.svg?react";
+import EyeOff from "./assets/eyeOff.svg?react";
+import { EyeOutlined } from "@ant-design/icons";
 
 const icons = {
   logo: Logo,
+  eye: EyeOutlined,
+  eyeOff: EyeOff,
 } as const;
 
 export type IconName = keyof typeof icons;
 
-type IconProps = {
+type CommonIconProps = {
   name: IconName;
   size?: number;
   color?: string;
-} & SVGProps<SVGSVGElement>;
+} & HTMLAttributes<HTMLElement>; // без SVGProps
 
-export const Icon = ({ name, size = 24, color, ...props }: IconProps) => {
+export const Icon = ({
+  name,
+  size = 24,
+  color,
+  style,
+  ...props
+}: CommonIconProps) => {
   const IconComponent = icons[name];
 
-  return <IconComponent width={size} height={size} color={color} {...props} />;
+  if (name === "logo") {
+    // SVG `?react`
+    const Svg = IconComponent as ComponentType<SVGProps<SVGSVGElement>>;
+    return (
+      <Svg
+        width={size}
+        height={size}
+        color={color}
+        {...(props as SVGProps<SVGSVGElement>)}
+      />
+    );
+  }
+
+  // ant-design icon
+  const AntIcon = IconComponent as typeof EyeOutlined;
+  return <AntIcon style={{ fontSize: size, color, ...style }} {...props} />;
 };
