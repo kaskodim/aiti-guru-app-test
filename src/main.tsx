9bd0f08a019673aa14357ba77dbef1d5100bd9fd
DmitriyGulevich <kaskodim@gmail.com>
2026-04-04 17:44:49 +0300

    подключает глобальный стили + шрифт

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+import "./styles/global.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
