ff3f56e09a32e4f9a6d0541e64407ee68849d314
DmitriyGulevich <kaskodim@gmail.com>
2026-04-06 11:54:02 +0300

    прогон претиер

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module '*.svg?react' {
-  import React from 'react';
+declare module "*.svg?react" {
+  import React from "react";
   const component: React.FC<React.SVGProps<SVGSVGElement>>;
   export default component;
 }
